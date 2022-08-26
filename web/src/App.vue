040a6c7e7ac1ffd0b1d494ecb805b73d07628eb4
Chengpeng Dong <964386385@qq.com>
2022-08-26 14:01:49 -0500

    mysql、注册模块 下

--- web/src/App.vue
+++ web/src/App.vue
@@ -7,10 +7,30 @@
 import Navbar from './components/NavBar.vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap"
+//import $ from 'jquery'
 
 export default {
   components: {
     Navbar
+  },
+  setup() {
+
+
+    // $.ajax({
+    //   url: "http://localhost:3000/user/account/register/",
+    //   type: "post",
+    //   data: {
+    //     username: "c",
+    //     password: "pc",
+    //     confirmedPassword: "pc",
+    //   },
+    //   success(resp) {
+    //     console.log(resp);
+    //   },
+    //   error(resp) {
+    //     console.log(resp);
+    //   }
+    // });
   }
 }
 </script>
